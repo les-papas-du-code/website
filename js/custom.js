909c12528a00685b737ecc99536cd103bc1040cf
Cyril Bareme <cyril.bareme@gmail.com>
2015-06-29 11:22:15 +0000

    change http in https (google maps)

--- js/custom.js
+++ js/custom.js
@@ -1082,7 +1082,7 @@ function appendBootstrap() {
     "use strict";
     var script = document.createElement("script");
     script.type = "text/javascript";
-    script.src = "http://maps.google.com/maps/api/js?sensor=false&callback=initialize";
+    script.src = "https://maps.google.com/maps/api/js?sensor=false&callback=initialize";
     document.body.appendChild(script);
 }
 
